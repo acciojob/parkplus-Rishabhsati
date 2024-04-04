869e5b53729ff114a4b8f6a9fd11930ac38b3ef9
Rishabh sati <rishabhsati6505@gmail.com>
2024-04-04 15:52:13 +0530

    write some apis

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -9,6 +9,8 @@ import com.driver.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -18,11 +20,59 @@ public class ReservationServiceImpl implements ReservationService {
     @Autowired
     SpotRepository spotRepository3;
     @Autowired
-    ReservationRepository reservationRepository3;
-    @Autowired
     ParkingLotRepository parkingLotRepository3;
     @Override
-    public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+    public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours,Integer numberOfWheels) throws Exception {
+        ParkingLot parkingLot;
+        try {
+            parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+        }
+        catch (Exception e){
+            throw new Exception("Cannot make reservation");
+        }
 
+        User user;
+        try {
+            user = userRepository3.findById(userId).get();
+        }
+        catch (Exception e){
+            throw new Exception("Cannot make reservation");
+        }
+
+        List<Spot> potentialSpotList = new ArrayList<>();
+        for (Spot sp : parkingLot.getSpotList())
+            if (!sp.getOccupied()) {
+                int wheels;
+
+                if (sp.getSpotType() == SpotType.TWO_WHEELER)
+                    wheels = 2;
+                else if (sp.getSpotType() == SpotType.FOUR_WHEELER)
+                    wheels = 4;
+                else
+                    wheels = Integer.MAX_VALUE;
+
+                if (wheels >= numberOfWheels) {
+                    potentialSpotList.add(sp);
+                }
+            }
+        if (potentialSpotList.isEmpty())
+            throw new Exception("Cannot make reservation");
+
+        potentialSpotList.sort(Comparator.comparingInt(Spot::getPricePerHour));
+
+        Spot spot = potentialSpotList.get(0);
+        spot.setOccupied(true);
+
+        Reservation reservation = new Reservation();
+        reservation.setNumberOfHours(timeInHours);
+        reservation.setUser(user);
+        reservation.setSpot(spot);
+
+        user.getReservationList().add(reservation);
+
+        userRepository3.save(user);
+        spotRepository3.save(spot);
+
+        return reservation;
     }
 }
