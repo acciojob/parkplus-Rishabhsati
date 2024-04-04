5bbdef04e902f2e18d1cdc57d67bc5b7f2951d72
Rishabh sati <rishabhsati6505@gmail.com>
2024-04-04 16:46:57 +0530

    fixed some silly mistakes

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -32,9 +32,12 @@ public class ParkingLotServiceImpl implements ParkingLotService {
         Spot spot = new Spot();
         spot.setPricePerHour(pricePerHour);
 
-        if(numberOfWheels == 2) spot.setSpotType(SpotType.TWO_WHEELER);
-        else if(numberOfWheels == 4) spot.setSpotType(SpotType.FOUR_WHEELER);
-        else spot.setSpotType(SpotType.OTHERS);
+        if (numberOfWheels <= 2)
+            spot.setSpotType(SpotType.TWO_WHEELER);
+        else if (numberOfWheels <= 4)
+            spot.setSpotType(SpotType.FOUR_WHEELER);
+        else
+            spot.setSpotType(SpotType.OTHERS);
 
         ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
         spot.setParkingLot(parkingLot);
